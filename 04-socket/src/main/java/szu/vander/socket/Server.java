01a1dd3c41282f88b4158a216226b4abebc977c7
蔡伟杰 <caiwj@chinamobilesz.com>
2018-09-11 19:01:24 +0800

    添加04章节：Socket和NioSocket

---
+++ 04-socket/src/main/java/szu/vander/socket/Server.java
@@ -0,0 +1,38 @@
+package szu.vander.socket;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+* @author      : Vander
+* @date        : 2018-09-11
+* @description ： 
+*/
+public class Server {
+    public static void main(String args[]) {
+        try {
+            //创建一个ServerSocket监听8080端口
+            ServerSocket server = new ServerSocket(8080);
+            //等待请求
+            Socket socket = server.accept();
+            //接收到请求后使用socket进行通信，创建BufferedReader用于读取数据，
+            BufferedReader is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String line = is.readLine();
+            System.out.println("received from client: " + line);
+            //创建PrintWriter，用于发送数据
+            PrintWriter pw = new PrintWriter(socket.getOutputStream());
+            pw.println("received data: " + line);
+            pw.flush();
+            //关闭资源
+            pw.close();
+            is.close();
+            socket.close();
+            server.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
